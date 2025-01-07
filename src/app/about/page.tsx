60923f73bc7f4f43fcd85ff5eff3c817390e1346
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-01-08 01:48:20 +0530

    Changed About page components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header'
+import LandingPage from '@/components/About/LandingPage'
 
 export default function About() {
     return (
         <div className='select-none min-h-screen min-w-screen flex items-center justify-center bg-primary overflow-hidden text-white'>
             <Header />
+            <LandingPage />
         </div>
     )
 }
